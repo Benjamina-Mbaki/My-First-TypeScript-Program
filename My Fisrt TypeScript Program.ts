379c48da3d0e06dc5c18ed2f2388ca234bfb0bba
Benjamina Mbaki <benjaminambaki@gmail.com>
2026-07-30 20:42:26 +0200

    Add files via upload

---
+++ My Fisrt TypeScript Program.ts
@@ -0,0 +1,14 @@
+// My First TypeScript Program
+
+let name: string = "Benjamina";
+let age: number = 20;
+
+console.log("=================================");
+console.log("   MY FIRST TYPESCRIPT PROGRAM");
+console.log("=================================");
+
+console.log("Hello! My name is " + Benjamina);
+console.log("I am " + 20 + " years old.");
+console.log("My favourite colour is Lavender.");
+console.log("I enjoy learning Java, JavaScript, TypeScript, C++, and Python.");
+console.log("Thank you for reading my first TypeScript program! 💙");
